2c83b2fa5cfdfa81f8fc332067732f0b21d5d733
Luca Cernuzzi <Luca@MacBook-Pro-de-Luca-Cernuzzi-2.local>
2016-11-28 09:08:44 -0300

    Clean project

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+
 var server = require("./server");
 var router = require("./router");
 var config = require('./config');
 var requestHandlers = require("./requestHandlers");
 
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-//var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
  
-
 var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
