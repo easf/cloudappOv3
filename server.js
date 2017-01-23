55156d98406ed35aba0fe92e1aeaedef4bbc545c
Luca Cernuzzi <Luca@MacBook-Pro-de-Luca-Cernuzzi-2.local>
2017-01-23 11:46:06 -0300

    Edit this.address to server

--- server.js
+++ server.js
@@ -4,30 +4,27 @@ var router = require("./router");
 
 
 function start(server_port, server_ip_address) {
-	
-    if (typeof server_ip_address === "undefined") {
-        //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
-        //  allows us to run/test the app locally.
-        console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
-    };
 
-    var app = express();
+  var app = express();
 	//router.route();
 	app.use("/", router);
-//  if ( server_ip_address === "127.0.0.1"){
-  app.listen( server_port, server_ip_address, function() {
-                console.log('%s: Node server started on %s:%s ...',
-                            Date(Date.now() ), app.port, app.address);
-                 console.log(server_ip_address);
-            }); 
 
 
- // }else{
-  //   app.listen( server_port, server_ip_address, function() {
-  //               console.log('%s: Node server started on %s:%d ...',
-  //                           Date(Date.now() ), server_port, server_ip_address);
-  //           });    
-  // }
+  if (typeof server_ip_address === "undefined") {
+        //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+        //  allows us to run/test the app locally.
+        //console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+        app.listen (server_port, function () {
+            var address = this.address()
+            console.log('%s worker %d running on http://%s:%d', APPNAME, process.pid, address.address, address.port)
+          });
+
+    } else {
+      app.listen( server_port, server_ip_address, function() {
+                console.log('%s: Node server started on %s:%d ...',
+                            Date(Date.now() ), server_port, server_ip_address);
+      });
+  }
 	
 }
 
