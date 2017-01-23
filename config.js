3b939af88281aa30b15dfe45d60bf2ba18b5f5d8
Luca Cernuzzi <Luca@MacBook-Pro-de-Luca-Cernuzzi-2.local>
2017-01-23 11:49:38 -0300

    Edit this.address to server

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = { }
 
 config.server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-config.server_ip_address = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+config.server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
 
 module.exports = config;
 
