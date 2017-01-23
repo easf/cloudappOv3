29b2d69f25f8ff2fc6b013703e737b79435145e1
Luca Cernuzzi <Luca@MacBook-Pro-de-Luca-Cernuzzi-2.local>
2017-01-23 09:34:36 -0300

    Edit address assignment to server

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ function start(server_port, server_ip_address) {
 	app.use("/", router);
 
 	app.listen( server_port, function () {
-  								var address = this.address() || server_ip_address
+  								var address = this.address();
+                  address.address = address.address || server_ip_address;
   								console.log('%s worker %d running on http://%s:%d',
-    							APPNAME, process.pid, address.address, address.port)
+    							APPNAME, process.pid, address.address, address.port);
 		}, function() {
 	            console.log('%s: Node server started on %s:%d ...',
 	                        Date(Date.now() ), server_port, server_ip_address);
