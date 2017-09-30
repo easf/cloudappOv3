20e7492b137eab1914e805178d050fa35c1c457a
easf <esanchiz92@gmail.com>
2017-09-30 00:04:08 -0400

    Update configs to migrate to v3

--- config.js
+++ config.js
@@ -8,13 +8,13 @@ var config = { }
 	Port setting 
 */
 					 // case: ENV-VAR    // case: OPENSHIFT-ENV-VAR			  // default
-config.server_port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+config.server_port = process.env.PORT ||  8080;
 
 /**
 	IP Setting: Just for OPENSHIFT case, for other ones the ip is setted in server.js code
 */
 
-config.server_ip_address = process.env.OPENSHIFT_NODEJS_IP;
+config.server_ip_address = 0.0.0.0;//process.env.OPENSHIFT_NODEJS_IP;
 
 
 /**
